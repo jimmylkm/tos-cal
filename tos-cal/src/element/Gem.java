c333d97c5e49d04c894c0506c3df42138a60edaa
Jimmy Leung <jimmyleunglkm@gmail.com>
2015-06-23 14:56:26 +0800

    Trial 2

--- tos-cal/src/element/Gem.java
+++ tos-cal/src/element/Gem.java
@@ -1,20 +1,5 @@
 package element;
 
 public class Gem {
-	private String color;
-	private boolean isAmp;
 	
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-	public boolean isAmp() {
-		return isAmp;
-	}
-	public void setAmp(boolean isAmp) {
-		this.isAmp = isAmp;
-	}
-
 }
